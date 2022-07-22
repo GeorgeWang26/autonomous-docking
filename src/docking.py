f018b84d92bc4a4c34822113bc29fa86775257b8
George Wang <wzheng2013@gmail.com>
2022-07-22 15:35:58 -0400

    phase 2

--- src/docking.py
+++ src/docking.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from time import time
 
 from cv2 import phase
@@ -22,6 +23,8 @@ cy0 = 0
 x0 = 0
 y0 = 0
 alpha = 0
+direction = 0
+tag_visible = False
 is_docking = False
 phase_one = False
 first_time = False
@@ -64,11 +67,16 @@ at_detector = Detector(families='tag36h11',
 
 
 def detect_tag(msg):
-    global x0, y0, cx0, cy0, alpha
+    global x0, y0, cx0, cy0, alpha, tag_visible
     rect_frame = bridge.imgmsg_to_cv2(msg, "mono8")
     tags = at_detector.detect(rect_frame, True, new_cam_params, tag_size)
+    """
+    maybe will cause problem with split second false?
+    """
+    tag_visible = False
     for tag in tags:
         if tag.tag_id == "0":
+            tag.visible = True
             # in cv frame, top left (0,0), horizontal is x, vertical is y
             cx0 = tag.center[0]
             cy0 = tag.center[1]
@@ -90,19 +98,26 @@ rect_sub = rospy.Subscriber("/image_rect", Image, detect_tag, queue_size=1)
 
 
 def docking(event):
-    global cx0, w, alpha, cam_pub, cam_msg, bot_pub, bot_msg, is_docking, phase_one, first_time, bot_cam_together
+    global y0, cx0, w, alpha, cam_pub, cam_msg, bot_pub, bot_msg, is_docking, phase_one, first_time, bot_cam_together, direction
     if not is_docking:
         print("out")
         return
     if phase_one:
-        if (0.45 * w < cx0) and (cx0 < 0.55 * w):
+        if tag_visible and (0.45 * w < cx0) and (cx0 < 0.55 * w):
             if bot_msg.angular.z == 0:
                 if bot_cam_together:
                     """
-                    assuming when facing tag, and robot is on right side of tag, alpha > 0, want 90 +++ |alpha|
+                    ASSUMING when facing tag, and robot is on right side of tag, alpha > 0, WANT 90 +++ |alpha|
                     """
                     # to left spin camera, increase pan angle
                     cam_msg.pan.data += 90 + (alpha > 0) * math.abs(alpha)
+                    # used for phase two, in case tag is not visible
+                    """
+                    ASSUMING when facing tag, and robot is on right side of tag, alpha > 0, WANT -1
+                    so robot backoff -1 when at right, positive alpha
+                    and dirve forward 1 when at left, negative alpha
+                    """
+                    direction = alpha < 0
                     bot_cam_together = False
                 else:
                     # spin camera to face left
@@ -118,7 +133,8 @@ def docking(event):
             time.sleep(3)
         else:
             """
-            assuming robot turn RIGHT when z > 0
+            ASSUMING robot turn RIGHT when z > 0
+            report error if cant find tag after 360
             """
             bot_msg.angular.z = 0.2
             bot_pub.publish(bot_msg)
@@ -126,6 +142,25 @@ def docking(event):
 
     # phase two
     # crab walk with cam ALWAYS 90 deg left of bot until is linned up with y offset
+    # camera stay STILL
+    if phase_two:
+        if tag_visible:
+            if math.abs(alpha) < 5:
+                # y0 > 0 => tag is on left side of camera => robot need to drive backward => direction = -1
+                # y0 < 0 => tag is on right side of camera => robot need to drive forward => direction = 1
+                direction = -1 * y0
+                bot_msg.linear.x = direction * 0.3
+                bot_msg.angular.z = 0
+            else:
+                # tag is in vision, spin robot to make |alpha| < 5
+                bot_msg.linear = 0
+                bot_msg.angular = 0.2
+        else:
+            # drive blind based on previous direction, hope to dirve parallel to tag plane
+            bot_msg.linear = direction * 0.3
+            bot_msg.angular = 0
+        bot_pub.publish(bot_msg)
+        return
 
 """
 use a service call to active docking
