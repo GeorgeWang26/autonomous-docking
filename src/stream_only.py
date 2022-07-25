5d966fe8fccacfcb92190755fc4115603e8cfbdf
George Wang <wzheng2013@gmail.com>
2022-07-25 16:22:56 -0400

    stream_only 0

--- src/stream_only.py
+++ src/stream_only.py
@@ -7,13 +7,14 @@ from cv_bridge import CvBridge
 rospy.init_node("stream_only")
 pub = rospy.Publisher("/image_raw", Image, queue_size = 1)
 print("hi")
-# cam = cv2.VideoCapture(0)
-cam = cv2.VideoCapture("rtspsrc location=rtsp://192.168.42.120:554/snl/live/1/1 latency=0 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink")
+cam = cv2.VideoCapture(0)
+# cam = cv2.VideoCapture("rtspsrc location=rtsp://192.168.42.120:554/snl/live/1/1 latency=0 ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! appsink")
 print("done")
 bridge = CvBridge()
 
 while True:
     ret, frame = cam.read()
+    print(frame.shape)
     msg = bridge.cv2_to_imgmsg(frame, encoding = "bgr8")
     pub.publish(msg)
     cv2.imshow("stream", frame)
