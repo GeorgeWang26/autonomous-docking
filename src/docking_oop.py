677f74e2df31d7aa99cbd969bb411f0582b3e6b4
George Wang <wzheng2013@gmail.com>
2022-08-11 10:52:11 -0400

    p1 abort (too many spin & no tag at all) + verify charging by volt

--- src/docking_oop.py
+++ src/docking_oop.py
@@ -2,12 +2,17 @@ import rospy
 from auto_docking.msg import TagInfo
 from ptz_pkg.msg import PTZPosition
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from bunker_msgs.msg import BunkerStatus
+from scipy.spatial.transform import Rotation as R
+import math
 
 
 class Docking():
     def __init__(self):
+        # tag info update
         self.tag_visible = False
-        self.width = 0
+        # self.width = 0
         # self.cx = 0
         self.tx = 0
         self.ty = 0
@@ -18,12 +23,26 @@ class Docking():
         self.t_smooth_lock = False
         self.first_visible = False
 
+        # docking
         self.is_charging = False
         self.is_docking = False
         self.bot_cam_together = False
         self.direction = 0
         self.wait_alpha = True
         # self.alpha_lock = 0
+
+        # not seeing tag at all since p1, abort
+        self.cur_bot_ori = 999
+        self.start_ori = 999
+        self.start_ori_count = 0
+        self.ever_tag_visible = False
+
+        # charging status verify after 10min
+        self.cur_bot_volt = 0
+        self.old_volt = 0
+        self.check_volt_diff = False
+        self.cur_bot_odom_x = 0
+        self.cur_bot_odom_y = 0
 
         self.second_time = False
         self.phase_one = False
@@ -37,12 +56,34 @@ class Docking():
         self.cam_msg = PTZPosition()
 
         rospy.init_node("docking")
-        self.cam_pub = rospy.Publisher("/ptz/move", PTZPosition, queue_size=1)
-        self.bot_pub = rospy.Publisher("/vel_mux/cmd_vel", Twist, queue_size=1)
-        self.tag_info_sub = rospy.Subscriber("/apriltag_detection", TagInfo, self.tag_update, queue_size=1)
+        self.cam_pub = rospy.Publisher("/ptz/move", PTZPosition, queue_size = 1)
+        self.bot_pub = rospy.Publisher("/vel_mux/cmd_vel", Twist, queue_size = 1)
+        self.tag_info_sub = rospy.Subscriber("/apriltag_detection", TagInfo, self.tag_update, queue_size = 1)
+        self.odom_sub = rospy.Subscriber("/bunker_odom", Odometry, self.odom_updatem, queue_size = 1)
+        self.bot_status_sub = rospy.Subscriber("/bunker_status", BunkerStatus, self.bot_status_update, queue_size = 1)
         # 20hz cycle
         # time.sleep() inside the timer callback does not affect the main cycle such as tag detection
         rospy.Timer(rospy.Duration(0.05), self.docking)
+
+
+    def odom_update(self, msg):
+        self.cur_bot_odom_x = msg.pose.pose.position.x
+        self.cur_bot_odom_y = msg.pose.pose.postiion.y
+        quat = msg.pose.orientation
+        r = R.from_quat([quat.x, quat.y, quat.z, quat.w])
+        # result in [roll, pitch, yaw] order
+        # try zyx
+        euler = r.as_euler("zxy", degrees=True)
+        print("euler: ", euler)
+        if abs(self.cur_bot_ori - euler[2]) > 1:
+            self.cur_bot_ori = euler[2]
+            if abs(self.cur_bot_ori - self.start_ori) < 0.5:
+                self.start_ori_count += 1
+                print("at starting bot orientation again, count:", self.start_ori_count)
+
+
+    def bot_status_update(self, msg):
+        self.cur_bot_volt = msg.battery_voltage
 
 
     def tag_update(self, msg):
@@ -52,7 +93,7 @@ class Docking():
             self.first_visible = True
             return
 
-        self.width = msg.width
+        # self.width = msg.width
         # self.cx = msg.cx
 
         if self.t_smooth_lock:
@@ -83,6 +124,7 @@ class Docking():
         else:
             self.alpha_neg_count += 1
 
+        self.ever_tag_visible = True
         self.first_visible = False
         # only set tag_visible=True after all status are updated
         self.tag_visible = True
@@ -107,6 +149,12 @@ class Docking():
         self.bot_cam_together = True
         self.wait_alpha = True
         # self.alpha_lock = 0
+
+        self.start_ori = self.cur_bot_ori
+        self.start_ori_count = 0
+        self.ever_tag_visible = False
+
+        self.check_volt_diff = False
 
         self.phase_one = True
         self.phase_one_second_time = False
@@ -146,8 +194,16 @@ class Docking():
         if self.phase_one:
             print("ty:", self.ty, "      alpha:", self.alpha)
             self.t_smooth_lock = False
+            if not self.ever_tag_visible and self.start_ori_count >= 2:
+                self.is_docking = False
+                print("phase 1, spin for at least 2 cycles, no tag in vision at all, aborting now")
+                return
+            elif self.bot_cam_together and self.start_ori >= 4:
+                self.is_docking = False
+                print("phase 1, spin at least 4 times, still in first part of phase 1, likely its too far from the tag, abort now")
+                return
+
             # if self.tag_visible and (abs(self.cx - 0.5 * self.width) / self.width) < 0.03:
-            # if self.tag_visible and ((not self.second_time and abs(self.ty) < 0.05) or (self.second_time and abs(self.ty) < 0.05)):
             if self.tag_visible and abs(self.ty) < 0.05:
                 self.bot_msg.angular.z = 0
                 print("phase 1      stoping robot before spinning camera")
@@ -276,11 +332,11 @@ class Docking():
             if not self.tag_visible:
                 terminate = True
                 self.is_docking = False
-                print("\nphase 33333, abort docking, tag is not visible")
+                print("\nphase 3, abort docking, tag is not visible")
             elif self.is_charging:
                 # wont do anything for now
                 terminate = True
-                print("\nrobot is charging")
+                print("\nphase 3, robot is charging")
             elif self.tx < 0.765:
                 terminate = True
                 print("\ntoo close to station and still NOT charging")
@@ -303,20 +359,43 @@ class Docking():
         if self.phase_four:
             self.t_smooth_lock = False
             print("alpha:", self.alpha)
-            if not self.tag_visible:
-                self.bot_msg.angular.z = 0
+            terminate = False
+            if self.is_charging:
+                terminate = True
+                print("\nphase 4, robot is charging")
                 self.is_docking = False
-                print("\nphase 44444, abort docking, tag is not visible")
-            if abs(self.alpha) < 0.5:
-                self.bot_msg.angular.z = 0
-                self.phase_four = False
-                print("exiting phase 4")
-                self.is_docking = False
+            elif not self.tag_visible:
+                terminate = True
+                print("\nphase 4, abort docking, tag is not visible")
+            elif abs(self.alpha) < 0.5:
+                terminate = True
+                print("\nphase4, parallel now")
             else:
                 # turn left if face right side of tag
                 self.bot_msg.angular.z = 0.04 if self.alpha < 0 else -0.04
+            if terminate:
+                self.bot_msg.angular.z = 0
+                self.phase_four = False
+                self.is_docking = False
+                self.check_volt_diff = True
+                self.old_volt = self.cur_bot_volt
+                # change to 600s = 10min, test with 60s for now
+                rospy.Timer(rospy.Duration(60), self.verify_charging, oneshot = True)
+                print("exiting phase 4")
             self.bot_pub.publish(self.bot_msg)
             return
+
+    
+    def verify_charging(self, event):
+        print("verify status:", self.check_volt_diff, "old volt:", self.old_volt, "cur volt:", self.cur_bot_volt)
+        if self.check_volt_diff and (self.cur_bot_volt - self.old_volt) < 1:
+            start_x = self.cur_bot_odom_x
+            start_y = self.cur_bot_odom_y
+            while math.sqrt((self.cur_bot_odom_x - start_x) ** 2 + (self.cur_bot_odom_y - start_y) ** 2) < 1:
+                print("euclidean distance:", math.sqrt((self.cur_bot_odom_x - start_x) ** 2 + (self.cur_bot_odom_y - start_y) ** 2))
+                self.bot_msg.linear.x = 0.2
+                self.bot_pub.publish(self.bot_msg)
+            self.start_docking()
 
 
 if __name__ == "__main__":
